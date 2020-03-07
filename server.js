84f583783d2ab309ebe8975a6b549d4e060a43fe
Heather Spoelstra <hmspoelstra@gmail.com>
2020-03-07 14:34:17 -0500

    create user, log in, log out working - not secure

--- server.js
+++ server.js
@@ -1,22 +1,59 @@
 const express = require('express')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
-require('dotenv').config()
 const app = express()
+const mongoose = require('mongoose')
+const session = require('express-session')
+const methodOverride = require('method-override')
+require('dotenv').config()
+
+app.use(express.urlencoded({ extended: false}))
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  }))
+
+  app.use(methodOverride("_method"))
+
+  app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/outfitbuilderdb', { 
+    useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        currentUser: req.session.currentUser
+    });
+  })
+
+  app.get('/app', (req, res)=>{
+    if(req.session.currentUser){
+        res.render('app/index.ejs')
+    } else {
+        res.redirect('/sessions/new');
+    }
+})
+
+const userController = require('./controllers/users.js')
+app.use('/users', userController)
+
+const sessionsController = require('./controllers/sessions.js')
+app.use('/sessions', sessionsController)
+
 
 // Configuration
 const PORT = process.env.PORT
 const mongoURI = process.env.MONGO_URI
 
-// Middleware
-// allows us to use put and delete methods
-app.use(methodOverride('_method'))
-// parses info from our input fields into an object
-app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.send('index route')
-  })
+
+
+
 
 
 // Listen
